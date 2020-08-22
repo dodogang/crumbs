39b8e052588f3715daffbd48c37d0a7edc9468ad
Trikzon <trikzondev@gmail.com>
2020-08-22 17:35:01 -0400

    Change license to MIT

--- src/main/java/net/dodogang/crumbs/block/CrumbsCraftingTableBlock.java
+++ src/main/java/net/dodogang/crumbs/block/CrumbsCraftingTableBlock.java
@@ -1,16 +1,3 @@
-/*
- * Copyright 2020 Day Dreamers Dev
- *
- * Crumbs is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * https://www.gnu.org/licenses/
- *
- * File: CrumbsCraftingTableBlock.java
- * Date: 2020-06-06 "v0.0.2 First Commit"
- * Revision:
- */
 package net.dodogang.crumbs.block;
 
 import net.dodogang.crumbs.container.CrumbsWorkbenchContainer;

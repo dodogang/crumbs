87c67f9ce68fa778344c4c34bffa13753690592e
Trikzon <trikzondev@gmail.com>
2020-11-10 18:12:07 -0500

    Fix barrel recipe conflicts

---
+++ src/main/java/net/dodogang/crumbs/mixin/RecipeManagerMixin.java
@@ -0,0 +1,39 @@
+package net.dodogang.crumbs.mixin;
+
+import com.google.gson.Gson;
+import net.dodogang.crumbs.Crumbs;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeManager;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.resource.JsonDataLoader;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mixin(RecipeManager.class)
+public abstract class RecipeManagerMixin extends JsonDataLoader {
+    private RecipeManagerMixin(Gson gson, String dataType) { super(gson, dataType); }
+
+    @Shadow public abstract <C extends Inventory, T extends Recipe<C>> List<T> getAllMatches(RecipeType<T> type, C inventory, World world);
+
+    // Redirects the getFirstMatch call in updateResult so we can handle the logic here.
+    // This is to get around recipe conflicts with vanilla barrels, and possibly more in the future.
+    // Now the crafting table will always choose a crumbs recipe in case of a conflict.
+    @Inject(method = "getFirstMatch", at = @At("HEAD"), cancellable = true)
+    private <C extends Inventory, T extends Recipe<C>> void
+    crumbs_onGetFirstMatch(RecipeType<T> type, C inventory, World world, CallbackInfoReturnable<Optional<T>> cir) {
+        Optional<T> firstCrumbsMatch = this.getAllMatches(type, inventory, world).stream()
+                .filter(t -> t.getId().getNamespace().equals(Crumbs.MOD_ID))
+                .findFirst();
+        if (firstCrumbsMatch.isPresent()) {
+            cir.setReturnValue(firstCrumbsMatch);
+        }
+    }
+}

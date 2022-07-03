8bb1b6d34c77e9fe37cdae49afefde5eb1b304a8
Dion Tryban <github@diontryban.com>
2022-07-02 21:04:42 -0400

    Add javadocs for CreativeModeTabBuilder
    This should have been done in the pr. Oops

--- Common/src/main/java/net/dodogang/ash/item/api/CreativeModeTabBuilder.java
+++ Common/src/main/java/net/dodogang/ash/item/api/CreativeModeTabBuilder.java
@@ -12,6 +12,14 @@ import java.util.function.Supplier;
 public abstract class CreativeModeTabBuilder {
     private static final CreativeModeTabBuilder IMPL = ImplLoader.load(CreativeModeTabBuilder.class);
 
+    /**
+     * Creates a {@link Builder} that helps with creating a platform's
+     * extension of {@link CreativeModeTab}.
+     *
+     * @param resLoc the {@link ResourceLocation} will become the name of the
+     *               {@link CreativeModeTab} and will be used for the translation key
+     * @return a {@link Builder}
+     */
     public static Builder create(ResourceLocation resLoc) {
         return IMPL.createImpl(resLoc);
     }
@@ -27,16 +35,34 @@ public abstract class CreativeModeTabBuilder {
             this.resLoc = resLoc;
         }
 
+        /**
+         * Adds an icon to the {@link CreativeModeTab}.
+         *
+         * @param iconSupplier the supplier should return the {@link ItemStack}
+         *                     that you wish to show on the tab
+         * @return this
+         */
         public Builder icon(Supplier<ItemStack> iconSupplier) {
             this.iconSupplier = iconSupplier;
             return this;
         }
 
+        /**
+         * Adds a custom list of items to be displayed in a tab, such as items
+         * with enchantments, potions, or other NBT values.
+         *
+         * @param stacksForDisplay add {@link ItemStack}s to this list to show
+         *                         in the {@link CreativeModeTab}
+         * @return this
+         */
         public Builder appendItems(Consumer<List<ItemStack>> stacksForDisplay) {
             this.stacksForDisplay = stacksForDisplay;
             return this;
         }
 
+        /**
+         * @return a new {@link CreativeModeTab}
+         */
         public abstract CreativeModeTab build();
     }
 }
